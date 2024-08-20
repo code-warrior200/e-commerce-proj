c831a58aebaeab88f241501181a1eed8492b19a7
Code~warrior200 <61137642+code-warrior200@users.noreply.github.com>
2024-08-20 06:39:41 -0700

    git commit -m

--- ecommerce_app/lib/Screens/home_screen.dart
+++ ecommerce_app/lib/Screens/home_screen.dart
@@ -78,8 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => UserProfileScreen(),
-        ),
+          builder: (context) => UserProfileScreen(),),
       );
       break;
   }
